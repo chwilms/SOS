a27a5528907ed09ccc212f84db7ed0ebec1ca190
chwilms <28784045+chwilms@users.noreply.github.com>
2024-09-23 01:15:59 +0200

    Update generate_contour_prompts.py

--- prompt_generation/generate_contour_prompts.py
+++ prompt_generation/generate_contour_prompts.py
@@ -68,9 +68,9 @@ def launch(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Script for generating prompts based on Contour object prior.')
-    parser.add_argument('edge_images', type=str, required=True,
+    parser.add_argument('edge_images', type=str,
                         help="Path to edge image directory, e.g., generated from COCO's train2017 images.")
-    parser.add_argument('prompt_path', type=str, required=True,
+    parser.add_argument('prompt_path', type=str,
                         help='Path to outfile (.json) for generated prompts.')
     parser.add_argument('--samples', dest='num_samples', default=50,
                         help='Maximum number of prompts per image. Parameter S in the paper.')
